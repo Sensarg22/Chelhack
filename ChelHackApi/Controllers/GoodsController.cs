afa60fed6999f3c2dbc42c3e74b8fb1f1c4e58d1
Mikhail Ivanov <tranquilized.mind@gmail.com>
2019-11-30 14:02:33 +0500

    Renamed project

---
+++ ChelHackApi/Controllers/GoodsController.cs
@@ -0,0 +1,11 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChelHackApi.Controllers
+{
+    [Route("[controller]")]
+    public class GoodsController : Controller
+    {
+        
+    }
+}
--- ChelHackApi/Program.cs
+++ ChelHackApi/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace ChalHackApi
+namespace ChelHackApi
 {
     public class Program
     {
