248cfe68e7bb15a8381ebd68a9b9643e156e2ad2
Mikhail Pidzhakov <sensarg22@gmail.com>
2019-11-30 12:17:43 +0500

    index

---
+++ ChelHackWeb/Pages/black-friday/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChelHackWeb.Pages.black_friday
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
